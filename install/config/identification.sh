34a17e90576981d044b445df7b2a1e855bbcc8f7
Carlos J. Torres <vlaadbrain@gmail.com>
2025-08-11 21:09:07 -0400

    more files with OMARCHY in them

--- install/config/identification.sh
+++ install/config/identification.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export OMARCHY_USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name> ")
-export OMARCHY_USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
+export GRAYHAT_USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name> ")
+export GRAYHAT_USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -2,6 +2,6 @@
 
 yay -S --noconfirm --needed ttf-font-awesome ttf-cascadia-mono-nerd ttf-ia-writer noto-fonts noto-fonts-emoji
 
-if [ -z "$OMARCHY_BARE" ]; then
+if [ -z "$GRAYHAT_BARE" ]; then
   yay -S --noconfirm --needed ttf-jetbrains-mono noto-fonts-cjk noto-fonts-extra
 fi
