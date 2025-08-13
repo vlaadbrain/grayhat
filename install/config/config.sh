f9a6a8db1ff0fe2927eb24c326f4a0eb0e95c443
Carlos J. Torres <vlaadbrain@gmail.com>
2025-08-12 20:36:18 -0400

    add more git stuff

--- install/config/config.sh
+++ install/config/config.sh
@@ -31,8 +31,44 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.remotes "remote -v"
+git config --global alias.a "add --interactive"
+
+git config --global alias.pu "!f() { git push -u upstream --tag $1; }; f"
+git config --global alias.puf "!f() { git push -f -u upstream --tag $1; }; f"
+git config --global alias.po "!f() { git push -u origin --tag $1; }; f"
+git config --global alias.pof "!f() { git push -f -u origin --tag $1; }; f"
+git config --global alias.up "!f() { git fetch upstream && git merge upstream/$1; }; f"
+git config --global alias.sup "!f() { git fetch --recurse-submodules upstream && git merge upstream/$1; }; f"
+git config --global alias.op "!f() { git fetch origin && git merge origin/$1; }; f"
+git config --global alias.sop "!f() { git fetch --recurse-submodules origin && git merge origin/$1; }; f"
+git config --global alias.do "!f() { git branch -D $1 && git branch -D -r origin/$1; }; f"
+git config --global alias.du "!f() { git branch -D $1 && git branch -D -r upstream/$1; }; f"
+git config --global alias.pr "!f() { git fetch origin pull/$1/head:pr-$1 && git checkout pr-$1; }; f"
+git config --global alias.pur "!f() { git fetch upstream pull/$1/head:pr-$1 && git checkout pr-$1; }; f"
+git config --global alias.ls "!f() { git branch -l -r; }; f"
+git config --global alias.lga "log --graph --oneline --all --decorate"
+
+git config --global core.editor "/usr/bin/env nvim"
+
+git config --global color.branch auto
+git config --global color.status auto
+git config --global color.diff auto
+git config --global color.ui auto
+git config --global apply.whitespace fix
+git config --global merge.conflictstyle diff3
+git config --global diff.colorMoved default
+git config --global i18n.filesEncoding utf-8
 git config --global pull.rebase true
 git config --global init.defaultBranch master
+
+# add delta
+yay -S --noconfirm --needed git-delta
+
+git config --global core.pager delta
+git config --global interactive.diffFilter = "delta --color-only"
+git config --global delta.navigate true
+git config --global delta.side-by-side true
 
 # Set identification from install inputs
 if [[ -n "${GRAYHAT_USER_NAME//[[:space:]]/}" ]]; then
@@ -42,6 +78,8 @@ fi
 if [[ -n "${GRAYHAT_USER_EMAIL//[[:space:]]/}" ]]; then
   git config --global user.email "$GRAYHAT_USER_EMAIL"
 fi
+
+git config --global user.signingkey 'if [[ -f "~/.ssh/id_rsa.pub" ]]; then KEY=`cat ~/.ssh/id_rsa.pub`; else KEY=''; fi; $KEY'
 
 # Set default XCompose that is triggered with CapsLock
 tee ~/.XCompose >/dev/null <<EOF
